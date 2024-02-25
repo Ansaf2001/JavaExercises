6cf14f788473ac2d8b30d3a8dac0524f2f48ddd0
Ansaf2001 <ansafansar2001@gmail.com>
2024-02-25 19:19:29 +0530

    class 2

---
+++ src/com/Ansaf/JavaExercies/basic.java
@@ -0,0 +1,7 @@
+package com.Ansaf.JavaExercies;
+
+public class basic {
+    public static void main(String args[]){
+        System.out.println("Welcome to Java");
+    }
+}
---
+++ src/com/Ansaf/JavaExercies/command.java
@@ -0,0 +1,16 @@
+package com.Ansaf.JavaExercies;
+
+public class command {
+    public static void main(String[] args) {
+        for (int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
+//This is single line comment.
+/*
+This
+is
+Multi-line
+Comment
+*/
