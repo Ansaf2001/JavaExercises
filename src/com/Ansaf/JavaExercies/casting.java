0a14e22df162b8992a99e3f835c503ff6fcd6501
Ansaf2001 <ansafansar2001@gmail.com>
2024-02-25 21:52:17 +0530

    class 3

---
+++ src/com/Ansaf/JavaExercies/casting.java
@@ -0,0 +1,11 @@
+package com.Ansaf.JavaExercies;
+//  Type casting in JAVA
+import java.lang.*;
+public class casting {
+    public static void main(String[] args) {
+        int a=10;
+        double b=a;
+        System.out.println("Int : "+a);
+        System.out.println("Double : "+b);
+    }
+}
---
+++ src/com/Ansaf/JavaExercies/variables.java
@@ -0,0 +1,16 @@
+package com.Ansaf.JavaExercies;
+
+public class variables {
+    public static void main(String[] args) {
+        String name = "Ansaf";
+        int age = 22;
+        float percent = 24.42f;
+        char gender = 'M';
+        boolean Marrried = false;
+        System.out.println("Name : "+ name);
+        System.out.println("Age : "+ age);
+        System.out.println("Percent : "+ percent);
+        System.out.println("Gender : "+ gender);
+        System.out.println("Married : " + Marrried);
+    }
+}
